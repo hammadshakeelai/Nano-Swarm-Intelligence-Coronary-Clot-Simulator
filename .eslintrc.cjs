a2008f957e5dbd21643af187fdf018c308f2e1c4
hammadai <hammadshakeel61@gmail.com>
2026-04-21 00:00:50 +0500

    Create .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  ignorePatterns: ["dist", "node_modules"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
